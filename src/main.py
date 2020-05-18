1863eeb856099893e55cd6cdddb09aa7c389a91a
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2020-05-18 11:39:42 +0100

    Add a simple Python 3 script

---
+++ src/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# A trivial Python 3 script
+
+def main():
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
