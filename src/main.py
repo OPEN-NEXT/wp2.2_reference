3c708c638ed174e138cfe08fa48f6fa2b43e150d
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2020-05-19 16:42:05 +0100

    Update comment

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# A trivial Python 3 script
+# A Python 3 script that does a few simple operations
 
 def main():
     message: str = "Hello world"
