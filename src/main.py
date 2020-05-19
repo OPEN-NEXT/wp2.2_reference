b2209f08c6181cb0fe63447b3cca5fab281d6dae
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2020-05-19 20:03:44 +0100

    Add type hints

--- src/main.py
+++ src/main.py
@@ -5,22 +5,27 @@
 # A trivial Python 3 script
 
 
-def quadratic(a, b, c):
+from typing import Tuple
+
+
+def quadratic(a: float, b: float, c: float) -> Tuple[float, float]:
     """Solve a quadratic equation"""
-    x_1 = (-b + (b**2 - 4*a*c)**(1/2)) / (2*a)
-    x_2 = (-b - (b**2 - 4*a*c)**(1/2)) / (2*a)
+    x_1: float = (-b + (b**2 - 4*a*c)**(1/2)) / (2*a)
+    x_2: float = (-b - (b**2 - 4*a*c)**(1/2)) / (2*a)
 
-    return x_1, x_2
+    return (x_1, x_2)
 
 
-def main():
+def main() -> None:
     message: str = "Hello"
     print(message)
 
     # Solve an arbitrary quadratic equation
-    a = 11
-    b = 18
-    c = 7
+    a: float = 11
+    b: float = 18
+    c: float = 7
+    x_1: float
+    x_2: float
     x_1, x_2 = quadratic(a=a, b=b, c=c)
     print("Solving for x in the quadratic equation ax^2 + bx + c = 0 where")
     print("a = {0}\nb = {1}\nc = {2}".format(a, b, c))
