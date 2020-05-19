67d9cc35b61c231c1ac64fd3554e16d6c5a7caf1
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2020-05-19 19:43:39 +0100

    Make output clearer & clean up

--- src/main.py
+++ src/main.py
@@ -4,12 +4,14 @@
 
 # A trivial Python 3 script
 
+
 def quadratic(a, b, c):
     """Solve a quadratic equation"""
     x_1 = (-b + (b**2 - 4*a*c)**(1/2)) / (2*a)
     x_2 = (-b - (b**2 - 4*a*c)**(1/2)) / (2*a)
 
     return x_1, x_2
+
 
 def main():
     message: str = "Hello"
@@ -20,7 +22,11 @@ def main():
     b = 18
     c = 7
     x_1, x_2 = quadratic(a=a, b=b, c=c)
-    print(str(x_1) + " " + str(x_2))
+    print("Solving for x in the quadratic equation ax^2 + bx + c = 0 where")
+    print("a = {0}\nb = {1}\nc = {2}".format(a, b, c))
+    print("The two roots (x) are: {root_1:5.5} and {root_2:5.5}".format(
+        root_1=x_1, root_2=x_2))
+
 
 if __name__ == "__main__":
     main()
